9364e09a66e6158a2a9c21c2e6e6db193e3e270d
ksindicue-ux <ksindicue@unal.edu.co>
2026-06-17 10:51:10 -0500

    Primera version

---
+++ P1.py
@@ -0,0 +1,19 @@
+cantidad = int(input("ingrese el numero de productos a registrar: "))
+Nombres = []
+Precios = []
+for i in range(cantidad):
+    a=input("Nombre del producto: ")
+    b=int(input("Precio del producto: "))
+    if b<0:
+        print("precio invalido, Ingrese de nuevo:")
+        b=int(input("Precio del producto: "))
+    Nombres.append(a)
+    Precios.append(b)
+for i in range(len(Nombres)):
+    print(Nombres[i],Precios[i])
+total = 0
+for i in range(len(Precios)):
+    total += Precios[i]
+Promedio = total/len(Precios)
+print(f"El total es ",total)
+print(f"El promedio es ",Promedio)
